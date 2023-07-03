672e75420b3c5a8dcadec5458d50c78e935ace1d
Marcus Aspin <maspin@unilink.com>
2023-07-03 17:40:09 +0100

    PI-1207 Final information screens

--- server/routes/sentencePlan.ts
+++ server/routes/sentencePlan.ts
@@ -1,4 +1,4 @@
-import { type RequestHandler, Router } from 'express'
+import { Request, type RequestHandler, Response, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import { formatDate } from '../utils/utils'
@@ -11,6 +11,10 @@ export default function sentencePlanRoutes(router: Router, service: Services): R
     const sentencePlan = await service.sentencePlanClient.getSentencePlan(id)
     const caseDetails = await service.deliusService.getCaseDetails(sentencePlan.crn)
     return { caseDetails, sentencePlan }
+  }
+
+  async function errorMessage(page: string, text: string, req: Request, res: Response) {
+    res.render(`pages/sentencePlan/${page}`, { ...(await loadSentencePlan(req.params.id)), errorMessage: { text } })
   }
 
   get('/case/:crn', async function loadCaseSummary(req, res) {
@@ -70,6 +74,38 @@ export default function sentencePlanRoutes(router: Router, service: Services): R
       riskFactors: req.body['risk-factors'],
       protectiveFactors: req.body['protective-factors'],
     })
+    res.redirect(`/sentence-plan/${id}/summary`)
+  })
+
+  get('/sentence-plan/:id/your-decisions', async (req, res) => {
+    res.render('pages/sentencePlan/yourDecisions', await loadSentencePlan(req.params.id))
+  })
+
+  post('/sentence-plan/:id/your-decisions', async function updatePractitionerComments(req, res) {
+    const { id } = req.params
+    const practitionerComments = req.body['practitioner-comments']
+    if (practitionerComments.length === 0)
+      await errorMessage('Please enter your decisions', 'practitionerComments', req, res)
+    if (practitionerComments.length > 5000)
+      await errorMessage('Your decisions must be 5000 characters or less', 'practitionerComments', req, res)
+
+    const existingSentencePlan = await service.sentencePlanClient.getSentencePlan(id)
+    await service.sentencePlanClient.updateSentencePlan({ ...existingSentencePlan, practitionerComments })
+    res.redirect(`/sentence-plan/${id}/summary`)
+  })
+
+  get('/sentence-plan/:id/individuals-comments', async (req, res) => {
+    res.render('pages/sentencePlan/individualsComments', await loadSentencePlan(req.params.id))
+  })
+
+  post('/sentence-plan/:id/individuals-comments', async function updateIndividualsComments(req, res) {
+    const { id } = req.params
+    const individualComments = req.body['individual-comments']
+    if (individualComments.length > 5000)
+      await errorMessage("Individual's comments must be 5000 characters or less", 'individualsComments', req, res)
+
+    const existingSentencePlan = await service.sentencePlanClient.getSentencePlan(id)
+    await service.sentencePlanClient.updateSentencePlan({ ...existingSentencePlan, individualComments })
     res.redirect(`/sentence-plan/${id}/summary`)
   })
 
