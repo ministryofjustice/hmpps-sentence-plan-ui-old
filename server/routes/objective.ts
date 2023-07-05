fdcccef451ff75268c87f5d0b596f41821d499eb
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-07-05 16:41:25 +0100

    PI-1237 added delete objective

--- server/routes/objective.ts
+++ server/routes/objective.ts
@@ -119,6 +119,13 @@ export default function objectiveRoutes(router: Router, service: Services): Rout
     res.redirect(`/sentence-plan/${sentencePlanId}/summary`)
   })
 
+  get('/sentence-plan/:sentencePlanId/objective/:objectiveId/confirmDelete', async function deleteAction(req, res) {
+    const { sentencePlanId, objectiveId } = req.params
+    res.render('pages/sentencePlan/confirmDeleteObjective', {
+      ...(await loadObjective(sentencePlanId, objectiveId)),
+    })
+  })
+
   interface ErrorMessages {
     [key: string]: { text: string }
   }
