7f265cd1e84a996929a25be294bbe10e1490e24d
Marcus Aspin <maspin@unilink.com>
2023-07-12 15:46:02 +0100

    Testing

--- integration_tests/e2e/reviewAndStartPlan.cy.ts
+++ integration_tests/e2e/reviewAndStartPlan.cy.ts
@@ -17,6 +17,8 @@ context('Review and start plan', () => {
   beforeEach(() => {
     cy.visit('/sentence-plan/00000000-0000-0000-0000-000000000003/summary')
     Page.verifyOnPage(SummaryPage).startPlanLink().click()
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
     Page.verifyOnPage(ReviewAndStartPlan)
   })
 
