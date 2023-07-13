391867cba104ee4588cb943c31c843f342ec90bd
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-07-13 09:49:05 +0100

    PI-1240 sentence plan review page

--- integration_tests/e2e/reviewAndStartPlan.cy.ts
+++ integration_tests/e2e/reviewAndStartPlan.cy.ts
@@ -3,7 +3,6 @@ import SummaryPage from '../pages/summary'
 import ReviewAndStartPlan from '../pages/reviewAndStartPlan'
 
 context('Review and start plan', () => {
-  let summaryPage: SummaryPage
   beforeEach(() => {
     cy.task('reset')
     cy.task('stubSignIn')
@@ -17,13 +16,10 @@ context('Review and start plan', () => {
 
   beforeEach(() => {
     cy.visit('/sentence-plan/00000000-0000-0000-0000-000000000003/summary')
-    // summaryPage = Page.verifyOnPage(SummaryPage)
-    // summaryPage.startPlanLink().click()
     Page.verifyOnPage(ReviewAndStartPlan)
   })
 
   it('displays start plan button', () => {
-    summaryPage = Page.verifyOnPage(SummaryPage)
-    // cy.get('[data-qa="confirm-start-sentence-plan"]').should('contain', 'Start Plan')
+    Page.verifyOnPage(SummaryPage)
   })
 })
